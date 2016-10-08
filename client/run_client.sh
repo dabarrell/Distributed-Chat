058fd405cd08d20c45b18bd0d2214326cb1fb743
mmihira <mmihira.w@gmail.com>
2016-10-08 21:06:27 +1100

    args for client script

--- client/run_client.sh
+++ client/run_client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if mvn clean package; then
-  java -jar `dirname $0`/target/Client-1.0-jar-with-dependencies.jar -p 19999 -h 115.146.95.8 -i mihira -d
+  java -jar `dirname $0`/target/Client-1.0-jar-with-dependencies.jar -p 19999 -h 115.146.95.8 -i $1 -d
 fi
