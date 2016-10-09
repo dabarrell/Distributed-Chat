ddf5fbbddb636e3916244c4ce0bd46471af38215
mmihira <mmihira.w@gmail.com>
2016-10-09 13:55:45 +1100

    damnit bash

--- run_deamon.sh
+++ run_deamon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if mvn clean package; then
-  nohup java -jar -Dtinylog.writer=file -Dtinylog.writer.filename=log.txt -Dtinylog.level=trace target/ChatServer-1.0-jar-with-dependencies.jar -l "servers_remote.conf" -n "$1" &
+  nohup java -jar -Dtinylog.writer="$2" -Dtinylog.writer.filename=log.txt -Dtinylog.level=trace target/ChatServer-1.0-jar-with-dependencies.jar -l "servers_remote.conf" -n "$1" &
 fi
