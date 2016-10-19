4f3c963d98afb1ac995cf0eb783e2fea852768a6
David Barrell <david_barrell@bigpond.com>
2016-10-20 00:11:55 +1100

    Updated IP addresses for third instance

--- deploy_scripts/vps3-dbarrel-new-server.sh
+++ deploy_scripts/vps3-dbarrel-new-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "Setting up 115.146.89.137"
-ssh ubuntu@115.146.91.161 "bash -s $1 dev3 $2 new" < deploy.sh
+echo "Setting up 115.146.91.218"
+ssh ubuntu@115.146.91.218 "bash -s $1 dev3 $2 new" < deploy.sh
