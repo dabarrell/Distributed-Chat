144a37d21423e5530e3f7ad7f2a558bce4b0356c
mmihira <mmihira.w@gmail.com>
2016-10-19 22:40:24 +1100

    new server

--- deploy_scripts/vps2-mihira.sh
+++ deploy_scripts/vps2-mihira.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Setting up 115.146.89.137"
-ssh ubuntu@115.146.89.137 "bash -s $1 mihira2Nectar $2" < deploy.sh
+ssh ubuntu@115.146.89.137 "bash -s $1 mihira2Nectar $2 root" < deploy.sh
