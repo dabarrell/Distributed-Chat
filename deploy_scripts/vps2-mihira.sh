bc7ac62110599dc9f9aff74a198f8926489e55ea
mmihira <mmihira.w@gmail.com>
2016-10-20 12:44:52 +1100

    god

--- deploy_scripts/vps2-mihira.sh
+++ deploy_scripts/vps2-mihira.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Setting up 43.240.99.121"
-ssh ubuntu@43.240.99.121 "bash -s $1 s2 $2 root" < deploy.sh
+ssh ubuntu@115.146.85.217 "bash -s $1 s2 $2 root" < deploy.sh
--- deploy_scripts/vps3-dbarrel-new-server.sh
+++ deploy_scripts/vps3-dbarrel-new-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Setting up 43.240.99.123"
-ssh ubuntu@43.240.99.123 "bash -s $1 s3 $2 root" < deploy.sh
+ssh ubuntu@115.146.86.164 "bash -s $1 s3 $2 root" < deploy.sh
